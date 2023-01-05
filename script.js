0cc803082cf1a39dc06b7a5de58a907eebe88924
jeremu2907 <jeremu2907@gmail.com>
2023-01-04 18:16:53 -0600

    added more pictures and refactor code

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-$(document).ready(function() {
-    $('#slide').mousewheel(function(e, delta) {
-        this.scrollLeft -= (delta * 100);
-        e.preventDefault();
-    });
-});
-
 function clickAbout(){
     document.getElementById("about").scrollIntoView({behavior: "smooth"})
 }
@@ -16,3 +9,40 @@ function clickProject(){
 function clickContact(){
     document.getElementById("contact").scrollIntoView({behavior: "smooth"})
 }
+
+var i = 0;
+var pictureSRC = ["pictures/DJI_0031.jpg","pictures/DJI_0029.jpg","pictures/DJI_0030.jpg","pictures/DJI_0005.jpg","pictures/DJI_0050.jpg","pictures/DJI_0055.jpg","pictures/275733462_284343423778166_3525976417585391731_n.jpg", "pictures/DJI_0042.jpg","pictures/DJI_0027.jpg","pictures/DJI_0040.jpg"]
+
+const val = document.createElement("img");
+val.src = pictureSRC[i];
+val.id = "gallery"
+document.getElementById("works").appendChild(val)
+
+function moveRight(){
+    i++;
+    let tar = document.getElementById("gallery");
+    tar.style.opacity = "0";
+    setTimeout(()=>{
+        tar.src = pictureSRC[i%pictureSRC.length]
+        setTimeout(() =>{
+            tar.style.opacity = "1";
+        },100)
+    },800)
+}
+
+function moveLeft(){
+    if(i === 0)
+        i = pictureSRC.length - 1;
+    else
+        i--;
+    let tar = document.getElementById("gallery");
+    tar.style.opacity = "0";
+    setTimeout(()=>{
+        tar.src = pictureSRC[i%pictureSRC.length]
+        setTimeout(() =>{
+            tar.style.opacity = "1";
+        },100)
+    },800)
+}
+
+const autoGallery = setInterval(moveRight, 5000);
